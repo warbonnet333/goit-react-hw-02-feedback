68867376802c58e2bb41b24ce6dc81c06a7005bc
warbonnet333 <warbonnet333@gmail.com>
2020-02-25 12:49:50 +0200

    fixed StatisticItem

--- src/components/StatisticItem/StatisticItem.jsx
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -2,17 +2,15 @@ import React from "react"
 import PropTypes from 'prop-types';
 import styles from "./styles.module.css"
 
-const StatisticItem = ({ good, neutral, bad, total, posPersent }) => {
-  return (
-    <ul className={styles.list}>
-      <li>Good: {good}</li>
-      <li>Neutral: {neutral}</li>
-      <li>Bad: {bad}</li>
-      <li>Total: {total}</li>
-      <li>Positive feedback: {posPersent}%</li>
-    </ul>
-  )
-}
+const StatisticItem = ({ good, neutral, bad, total, posPersent }) => (
+  <ul className={styles.list}>
+    <li>Good: {good}</li>
+    <li>Neutral: {neutral}</li>
+    <li>Bad: {bad}</li>
+    <li>Total: {total}</li>
+    <li>Positive feedback: {posPersent}%</li>
+  </ul>
+)
 
 
 StatisticItem.propTypes = {
